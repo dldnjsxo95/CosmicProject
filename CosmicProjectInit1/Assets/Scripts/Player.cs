80da57c97b6dc3adf68c0afc507ea501e0ee8655
ActNumber14 <68317209+ActNumber14@users.noreply.github.com>
2020-07-28 13:40:15 +0900

    이동 및 트리거작용

--- CosmicProjectInit1/Assets/Scripts/Player.cs
+++ CosmicProjectInit1/Assets/Scripts/Player.cs
@@ -42,13 +42,20 @@ public class Player : MonoBehaviour
 
     }
 
-    private void Move()
-    {
-        float h = Input.GetAxis("Horizontal");
-        Vector3 dir = new Vector3(h, 0, 0);
-        dir.Normalize();
+    //private void Move()
+    //{
+    //    float h = Input.GetAxis("Horizontal");
+    //    Vector3 dir = new Vector3(h, 0, 0);
+    //    dir.Normalize();
 
-        transform.position += dir * speed * Time.deltaTime;
+    //    transform.position += dir * speed * Time.deltaTime;
+    //}
+
+    //마우스 드래그에 따라 움직이는 플레이어 객체
+    private void OnMouseDrag()
+    {
+        Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 3.6f);
+        this.transform.position = Camera.main.ScreenToWorldPoint(mousePos);
     }
 
     private void ChangeColor()
@@ -57,22 +64,23 @@ public class Player : MonoBehaviour
         else playerColor.material.color = Color.blue;
     }
 
-    //void OnTriggerEnter(Collider other)
-    //{
-    //    if (other.gameObject.tag == "Notes")
-    //    {
-    //        print("123");
-    //        other.gameObject.SetActive(false);
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Notes")
+        {
+            print("123");
+            other.gameObject.SetActive(false);
 
 
-    //    }
-    //}
+        }
+    }
+
     //void OnCollisionEnter(Collision other)
     //{
     //    if (other.gameObject.tag == "Notes")
     //    {
     //        print("123");
-    //        other.gameObject.SetActive(false)
+    //        other.gameObject.SetActive(false);
 
     //    }
     //}

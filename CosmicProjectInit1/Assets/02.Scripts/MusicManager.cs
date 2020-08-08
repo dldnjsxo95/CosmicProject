489c5f2c1c4553fabf31aed92e17ec022e06dfdc
dldnjsxo95 <41187977+dldnjsxo95@users.noreply.github.com>
2020-08-08 23:05:50 +0900

    .

--- CosmicProjectInit1/Assets/02.Scripts/MusicManager.cs
+++ CosmicProjectInit1/Assets/02.Scripts/MusicManager.cs
@@ -1,51 +1,65 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MusicManager : MonoBehaviour
 {
 	public enum Music
 	{
-        NeverBeLikeU,
-        DontStartNow,
-        Older,
-    }
-    public static Music music;
+		NeverBeLikeU,
+		DontStartNow,
+		Older,
+	}
+	public static Music music;
+	public GameObject ReadSignal;
+	public GameObject SelectUI;
 
-    public static Music MUSIC
+	public Button BntDontStart;
+	public Button BntNever;
+	public Button BntOlder;
+
+
+	public static Music MUSIC
 	{
 		get { return music; }
-        set { music = value; }
+		set { music = value; }
 	}
 
-    public GameObject ReadSignal;
+	// Start is called before the first frame update
+	void Start()
+	{
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.Alpha1))
-		{
-            music = Music.NeverBeLikeU;
-            ReadSignal.SetActive(true);
-		}
+	// Update is called once per frame
+	void Update()
+	{
+		BntDontStart.onClick.AddListener(Dont);
+		BntDontStart.onClick.AddListener(Never);
+		BntDontStart.onClick.AddListener(Older);
 
-        if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            music = Music.DontStartNow;
-            ReadSignal.SetActive(true);
-        }
+	}
 
-        if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            music = Music.Older;
-            ReadSignal.SetActive(true);
-        }
+	void Dont()
+	{
+		music = Music.DontStartNow;
+		ReadSignal.SetActive(true);
+		SelectUI.SetActive(false);
+	}
 
-    }
+	void Never()
+	{
+		music = Music.NeverBeLikeU;
+		ReadSignal.SetActive(true);
+		SelectUI.SetActive(false);
+	}
+
+	void Older()
+	{
+		music = Music.Older;
+		ReadSignal.SetActive(true);
+		SelectUI.SetActive(false);
+	}
+
 }

91bb5765ede482d136c29358480c28689c7af384
Namil0603 <qqa0603@naver.com>
2020-08-12 17:53:55 +0900

    .

--- CosmicProjectInit1/Assets/02.Scripts/Destination.cs
+++ CosmicProjectInit1/Assets/02.Scripts/Destination.cs
@@ -4,31 +4,35 @@ using UnityEngine;
 
 public class Destination : MonoBehaviour
 {
-    public float speed;
-    public float MinX;
-    public float MaxX;
-    public float startDir;
+	public Transform player;
+	public float forward;
+	public float down;
 
-    Vector3 dir;
-    // Start is called before the first frame update
-    void Start()
-    {
-        dir = Vector3.right *startDir;
-    }
+	public float speed;
+	public float MinX;
+	public float MaxX;
+	public float startDir;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if(transform.position.x < MinX)
+	Vector3 dir;
+	// Start is called before the first frame update
+	void Start()
+	{
+		dir = Vector3.right * startDir;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (transform.position.x < MinX)
 		{
-            dir = Vector3.right;
+			dir = Vector3.right;
 		}
 
-        if(transform.position.x > MaxX)
+		if (transform.position.x > MaxX)
 		{
-            dir = Vector3.left;
+			dir = Vector3.left;
 		}
 
-        transform.position += dir.normalized * speed * Time.deltaTime; 
-    }
+		transform.position += dir.normalized * speed * Time.deltaTime;
+	}
 }
